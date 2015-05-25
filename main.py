188ae0b76f829544b1991e3c12ea1c4ea0455789
caryando <caryando@gmail.com>
2015-05-24 22:26:59 -0700

    Add the iterative version of algorithm 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import math
+import time
 
 
 def merge(left, right):
@@ -43,66 +44,10 @@ def swap(array, a, b):
     array[a], array[b] = array[b], array[a]
 
 
-def partition1(array, left=0, right=0):
-    if right == 0:
-        right = len(array) - 1
-    i = left
-    j = right
-    pivot = array[left + ((right - left) // 2)]
-    # pivot_index = random.randint(left, right)
-    #pivot = array[pivot_index]
-    #print('Pivot index = ' + str(pivot_index))
-    print('Picked pivot = ' + str(pivot))
-    while i <= j:
-        while array[i] < pivot:
-            i += 1
-        while array[j] > pivot:
-            j -= 1
-        if i <= j:
-            swap(array, i, j)
-            i += 1
-            j -= 1
-    return i
-
-
-def quicksort(alist):
-    quick_sort_helper(alist, 0, len(alist) - 1)
-
-
-def quick_sort_helper(alist, first, last):
-    if first < last:
-        split_point = partition(alist, first, last)
-
-        quick_sort_helper(alist, first, split_point - 1)
-        quick_sort_helper(alist, split_point + 1, last)
-
-
-def partition(alist, first, last, pivot_position=0):
-    swap(alist, first, pivot_position)
-    pivot = alist[first]
-
-    i = first + 1
-    j = last
-
-    finished = False
-    while not finished:
-        while i <= j and alist[i] <= pivot:
-            i += 1
-        while alist[j] >= pivot and j >= i:
-            j -= 1
-        if j < i:
-            finished = True
-        else:
-            temp = alist[i]
-            alist[i] = alist[j]
-            alist[j] = temp
-    swap(alist, first, j)
-    return j
-
-
-def partition2(arr, left, right, pivot_index):
+def partition(arr, left, right, pivot_index):
+    """ Partition the array relative to the given pivot. This function is in-place and uses O(1) space """
     pivot_value = arr[pivot_index]
-    swap(arr, pivot_index, right)
+    swap(arr, pivot_index, right)  # We use the last element as the pivot placeholder
     store_index = left
     for i in range(left, right):
         if arr[i] < pivot_value:
@@ -112,7 +57,37 @@ def partition2(arr, left, right, pivot_index):
     return store_index
 
 
+def quickselect_recursive(arr, left, right, k):
+    """ Algorithm #2: Select the kth element by recursively partitioning the array """
+    if left == right:
+        return arr[left]
+    pivot_index = left + math.floor(random.randint(0, right-left))
+    pivot_index = partition(arr, left, right, pivot_index)
+    if k == pivot_index:
+        return arr[k]
+    elif k < pivot_index:
+        return quickselect_recursive(arr, left, pivot_index - 1, k)
+    else:
+        return quickselect_recursive(arr, pivot_index + 1, right, k)
+
+
+def quickselect_iterative(arr, left, right, k):
+    """ Algorithm #2: Select the kth element by iteratively partitioning the array """
+    if left == right:
+        return arr[left]
+    while True:
+        pivot_index = left + math.floor(random.randint(0, right-left))
+        pivot_index = partition(arr, left, right, pivot_index)
+        if k == pivot_index:
+            return arr[k]
+        elif k < pivot_index:
+            right = pivot_index - 1
+        else:
+            left = pivot_index + 1
+
+
 def linear_select(arr, k):
+    """ Algorithm #3: Use the median-of-medians to select a better pivot to select the kth element """
     if len(arr) <= 5:
         return mergesort(arr)[k]
     else:
@@ -122,48 +97,138 @@ def linear_select(arr, k):
             sorteded = mergesort(i)
             medians.append(sorteded[len(sorteded)//2])
         median_of_medians = linear_select(medians, math.floor(len(medians) / 2))
-        val = arr.index(median_of_medians)
-        t = partition2(arr, 0, len(arr) - 1, val)
+        val = arr.index(median_of_medians)  # The index of the median of medians
+        t = partition(arr, 0, len(arr) - 1, val)
         if k == t:
             return arr[k]
         elif k < t:
             return linear_select(arr[:t], k)
         else:
             return linear_select(arr[t+1:], k - t - 1)
-    #return medians
 
 
-def quickselect(arr, left, right, k):
-    if left == right:
-        return arr[left]
-    pivot_index = left + math.floor(random.randint(0, right-left))
-    pivot_index = partition2(arr, left, right, pivot_index)
-    if k == pivot_index:
-        return arr[k]
-    elif k < pivot_index:
-        return quickselect(arr, left, pivot_index - 1, k)
-    else:
-        return quickselect(arr, pivot_index + 1, right, k)
-#    if left == right:
-#        return arr[left]
-#    while True:
-#        pivot_index = partition(arr, left, right)
-#        if k == pivot_index:
-#            return arr[k]
-#        elif k < pivot_index:
-#            right = pivot_index - 1
-#        else:
-#            left = pivot_index + 1
+def random_list(n):
+    return [random.randint(0, 2000) for i in range(n)]
 
 
 if __name__ == '__main__':
     test = [54, 26, 93, 17, 77, 31, 44, 55, 20, 3]
     t = [4, 3, 2, 1]
-    print(linear_select(test, 4))
-    print(quickselect(test, 0, len(test)-1, 4))
-    print(mergesort(test)[4])
-    #print('posit: ' + str(partition(test, 0, len(test)-1)))
-    #print(test)
-    #print('THE KTH ELEMENT IS' + str(quickselect(test, 0, len(test)-1, 0)))
-    #print(quickselect(alist, 2))
-    #print('\n\nsorted: ' + str(mergesort(test)))
+    print(linear_select(t, 3))
+    print(quickselect_recursive(t, 0, len(t)-1, 3))
+    print(mergesort(t)[3])
+
+    for i in range(1, 10):
+        length = 10 ** i
+        temp_array = random_list(length)
+        print("Array size: " + str(length))
+        print("Selecting 1st element")
+        a = time.time()
+        y = quickselect_recursive(temp_array, 0, len(temp_array)-1, 0)
+        b = time.time()
+        print("quickselect recursive took: " + str(b - a) + " seconds")
+        a = time.time()
+        y = quickselect_iterative(temp_array, 0, len(temp_array)-1, 0)
+        b = time.time()
+        print("quickselect iterative took: " + str(b - a) + " seconds")
+        a = time.time()
+        z = linear_select(temp_array, 0)
+        b = time.time()
+        print("MM approach took: " + str(b - a) + " seconds")
+        a = time.time()
+        x = mergesort(temp_array)[0]
+        b = time.time()
+        print("Mergesort approach took: " + str(b - a) + " seconds")
+        print("")
+
+        if x != y or y != z:
+            print("ERROR INCONSISTENT DATA")
+            break
+
+        print("Selecting 1/4th element")
+        a = time.time()
+        y = quickselect_recursive(temp_array, 0, len(temp_array)-1, len(temp_array)//4)
+        b = time.time()
+        print("quickselect_recursive took: " + str(b - a) + " seconds")
+        a = time.time()
+        y = quickselect_iterative(temp_array, 0, len(temp_array)-1, len(temp_array)//4)
+        b = time.time()
+        print("quickselect iterative took: " + str(b - a) + " seconds")
+        a = time.time()
+        z = linear_select(temp_array, len(temp_array)//4)
+        b = time.time()
+        print("MM approach took: " + str(b - a) + " seconds")
+        a = time.time()
+        x = mergesort(temp_array)[len(temp_array)//4]
+        b = time.time()
+        print("Mergesort approach took: " + str(b - a) + " seconds")
+        print("")
+
+        if x != y or y != z:
+            print("ERROR INCONSISTENT DATA")
+            break
+
+        print("Selecting 1/2 element")
+        a = time.time()
+        y = quickselect_recursive(temp_array, 0, len(temp_array)-1, len(temp_array)//2)
+        b = time.time()
+        print("quickselect_recursive took: " + str(b - a) + " seconds")
+        a = time.time()
+        y = quickselect_iterative(temp_array, 0, len(temp_array)-1, len(temp_array)//2)
+        b = time.time()
+        print("quickselect iterative took: " + str(b - a) + " seconds")
+        a = time.time()
+        z = linear_select(temp_array, len(temp_array)//2)
+        b = time.time()
+        print("MM approach took: " + str(b - a) + " seconds")
+        a = time.time()
+        x = mergesort(temp_array)[len(temp_array)//2]
+        b = time.time()
+        print("Mergesort approach took: " + str(b - a) + " seconds")
+        print("")
+
+        if x != y or y != z:
+            print("ERROR INCONSISTENT DATA")
+            break
+
+        print("Selecting 3/4th element")
+        a = time.time()
+        y = quickselect_recursive(temp_array, 0, len(temp_array)-1, (3*len(temp_array))//4)
+        b = time.time()
+        print("quickselect_recursive took: " + str(b - a) + " seconds")
+        a = time.time()
+        y = quickselect_iterative(temp_array, 0, len(temp_array)-1, (3*len(temp_array)//4))
+        b = time.time()
+        print("quickselect iterative took: " + str(b - a) + " seconds")
+        a = time.time()
+        z = linear_select(temp_array, (3*len(temp_array))//4)
+        b = time.time()
+        print("MM approach took: " + str(b - a) + " seconds")
+        a = time.time()
+        x = mergesort(temp_array)[(3*len(temp_array))//4]
+        b = time.time()
+        print("Mergesort approach took: " + str(b - a) + " seconds")
+        print("")
+
+        if x != y or y != z:
+            print("ERROR INCONSISTENT DATA")
+            break
+
+        print("Selecting last element")
+        a = time.time()
+        y = quickselect_recursive(temp_array, 0, len(temp_array)-1, len(temp_array) - 1)
+        b = time.time()
+        print("quickselect_recursive took: " + str(b - a) + " seconds")
+        a = time.time()
+        y = quickselect_iterative(temp_array, 0, len(temp_array)-1, len(temp_array) - 1)
+        b = time.time()
+        print("quickselect iterative took: " + str(b - a) + " seconds")
+        a = time.time()
+        z = linear_select(temp_array, len(temp_array) - 1)
+        b = time.time()
+        print("MM approach took: " + str(b - a) + " seconds")
+        a = time.time()
+        x = mergesort(temp_array)[len(temp_array) - 1]
+        b = time.time()
+        print("Mergesort approach took: " + str(b - a) + " seconds")
+        print("")
